4c4384ccdb8ee1b2ecf52ab9a8d89911e390c920
mrtambour <6724649+mrtambour@users.noreply.github.com>
2023-03-30 15:57:32 -0700

    finished creating architecture level structures

--- src/levels.rs
+++ src/levels.rs
@@ -25,6 +25,72 @@ impl V1 {
         }
     }
 }
-struct V2 {}
-struct V3 {}
-struct V4 {}
+struct V2 {
+    supports_cmpxchg16b: bool,
+    supports_lahf_sahf: bool,
+    supports_popcnt: bool,
+    supports_sse3_addsubpd: bool,
+    supports_sse4_1: bool,
+    supports_sse4_2: bool,
+    supports_sse3_phaddd: bool,
+}
+
+impl V2 {
+    pub fn new() -> V2 {
+        V2 {
+            supports_cmpxchg16b: false,
+            supports_lahf_sahf: false,
+            supports_popcnt: false,
+            supports_sse3_addsubpd: false,
+            supports_sse4_1: false,
+            supports_sse4_2: false,
+            supports_sse3_phaddd: false,
+        }
+    }
+}
+struct V3 {
+    supports_avx: bool,
+    supports_avx2: bool,
+    supports_bmi1: bool,
+    supports_bmi2: bool,
+    supports_f16c: bool,
+    supports_fma: bool,
+    supports_lzcnt: bool,
+    supports_movbe: bool,
+    supports_osxsave: bool,
+}
+
+impl V3 {
+    pub fn new() -> V3 {
+        V3 {
+            supports_avx: false,
+            supports_avx2: false,
+            supports_bmi1: false,
+            supports_bmi2: false,
+            supports_f16c: false,
+            supports_fma: false,
+            supports_lzcnt: false,
+            supports_movbe: false,
+            supports_osxsave: false,
+        }
+    }
+}
+struct V4 {
+    supports_avx512f: bool,
+    supports_avx512bw: bool,
+    supports_avx512cd: bool,
+    supports_avx512dq: bool,
+    supports_avx512vl: bool,
+}
+
+impl V4 {
+    pub fn new() -> V4 {
+        V4 {
+            supports_avx512f: false,
+            supports_avx512bw: false,
+            supports_avx512cd: false,
+            supports_avx512dq: false,
+            supports_avx512vl: false,
+        }
+    }
+}
